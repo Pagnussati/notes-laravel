9caf37676f81bffd59f1f78fe33a8ea9ff658cd8
Juaum <127996526+Pagnussati@users.noreply.github.com>
2024-11-25 10:34:08 -0300

    Validando o login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,8 +13,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text-username');
-        echo $request->input('text-password');
+        // Form Validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required',
+            ]
+        );
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo $username;
+        echo $password;
     }
 
     public function logout()
