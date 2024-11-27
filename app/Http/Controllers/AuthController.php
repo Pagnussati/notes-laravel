509a6f3121c423c6ad439848fd6cd341fe988aee
Juaum <127996526+Pagnussati@users.noreply.github.com>
2024-11-26 23:56:08 -0300

    Adicionando mais verificoes na validacao

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,9 +15,18 @@ class AuthController extends Controller
     {
         // Form Validation
         $request->validate(
+            // Rules
             [
-                'text_username' => 'required',
-                'text_password' => 'required',
+                'text_username' => 'required | email',
+                'text_password' => 'required | min:6 | max:16',
+            ],
+            // Error messages
+            [
+                'text_username.required' => 'O campo usuário é obrigatório.',
+                'text_username.email' => 'O email inserido é inválido.',
+                'text_password.required' => 'O campo senha é obrigatório.',
+                'text_password.min' => 'O campo senha deve ter no mínimo :min caracteres.',
+                'text_password.max' => 'O campo senha deve ter no máximo :max caracteres.',
             ]
         );
 
