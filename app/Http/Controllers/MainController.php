7b474e54d402616d6301553d8bac2388766b70d4
Juaum <127996526+Pagnussati@users.noreply.github.com>
2024-11-30 14:00:23 -0300

    Criando middlewares de verificacao de sessao

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        echo 'DALE PAU DALE NO DOUZE TUDO NOSSO E NADA DELES';
+    }
 
+    public function newNote()
+    {
+        // Create new note
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,21 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 
-// Auth routes
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+// Not logged routes
+Route::middleware([ CheckIsNotLogged::class ])->group(function() {
+    // Auth routes
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
+
+// Login required routes
+Route::middleware([ CheckIsLogged::class ])->group(function() {
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
