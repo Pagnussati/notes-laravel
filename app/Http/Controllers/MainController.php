f1616fd7afec4fd46d0ed0a2af56020262f788dd
Juaum <127996526+Pagnussati@users.noreply.github.com>
2024-12-11 15:19:23 -0300

    Adicionando funcionalidade de criar e editar nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
+use App\Services\Operations;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -19,6 +23,86 @@ class MainController extends Controller
 
     public function newNote()
     {
-        // Create new note
+        // Show new note
+        return view('new_note');
+    }
+
+    public function newNoteSubmit(Request $request)
+    {
+        $request->validate(
+            // Rules
+            [
+                'text_title' => 'required | min:3 | max:200',
+                'text_note' => 'required | min:3 | max:3000',
+            ],
+            // Error messages
+            [
+                'text_title.required' => 'O campo titulo é obrigatório.',
+                'text_title.min' => 'O campo titulo deve ter no minimo :min caracteres',
+                'text_title.max' => 'O campo titulo deve ter no maximo :max caracteres',
+
+                'text_note.required' => 'O campo nota é obrigatório.',
+                'text_note.min' => 'O campo nota deve ter no mínimo :min caracteres.',
+                'text_note.max' => 'O campo nota deve ter no máximo :max caracteres.',
+            ]
+        );
+
+        $id = session('user.id');
+
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
+    }
+
+    public function editNote($id)
+    {
+        $id = Operations::decryptId($id);
+
+        $note = Note::find($id);
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request)
+    {
+        $request->validate(
+            // Rules
+            [
+                'text_title' => 'required | min:3 | max:200',
+                'text_note' => 'required | min:3 | max:3000',
+            ],
+            // Error messages
+            [
+                'text_title.required' => 'O campo titulo é obrigatório.',
+                'text_title.min' => 'O campo titulo deve ter no minimo :min caracteres',
+                'text_title.max' => 'O campo titulo deve ter no maximo :max caracteres',
+
+                'text_note.required' => 'O campo nota é obrigatório.',
+                'text_note.min' => 'O campo nota deve ter no mínimo :min caracteres.',
+                'text_note.max' => 'O campo nota deve ter no máximo :max caracteres.',
+            ]
+        );
+
+        if($request->note_id == null) {
+            return redirect()->route('home');
+        }
+
+        $id = Operations::decryptId($request->note_id);
+
+        $note = Note::find($id);
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
+    }
+
+    public function deleteNote($id)
+    {
+        //$id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
     }
 }
